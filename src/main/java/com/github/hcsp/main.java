f8154a109bc9b5cf3dccede3e8d7bb589a05eb86
“idpeng” <630606938@qq.com>
2021-10-07 15:12:20 +0800

    设计并实现基本的算法
    这个提交中，实现了基本的爬虫算法
    虽然很原始，但是还想小步提交

--- src/main/java/com/github/hcsp/main.java
+++ src/main/java/com/github/hcsp/main.java
@@ -6,25 +6,93 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-/**
- * @author kangjio
- * @data 13:05
- */
 public class main {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("http://sina.cn");
 
-            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
+        //未处理的线程池
+        List<String> linkPoll = new ArrayList<>();
+        //已经处理的线程池
+        Set<String> processedLinks = new HashSet<>();
+        linkPoll.add("https://sina.cn");
 
-                System.out.println(EntityUtils.toString(entity1));
+        while (true) {
+            if (linkPoll.isEmpty()) {
+                break;
             }
+            //ArrayList从尾部删除更有效率
+            String link = linkPoll.remove(linkPoll.size() - 1);
 
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPoll::add);
+                storeIntoDatabaseIfItIsNewPage(doc);
+                processedLinks.add(link);
+            } else {
+                //这是不感兴趣的
+                continue;
+            }
         }
+    }
+
+    private static void storeIntoDatabaseIfItIsNewPage(Document doc) {
+        //假如这是一个新闻页面，就存入数据库，否则什么都不做
+        Elements articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        //这是感兴趣的
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36");
+
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewPage(link) || isIndexPage(link) && isNotLoginPage(link));
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
